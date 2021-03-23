0dffd2f0cce764c5b504b960894f3a13c13d964a
Kyle Smilon <smilonk@moravian.edu>
2021-03-23 10:50:13 -0400

    added 2nd Types

--- BuildNationSmilons.java
+++ BuildNationSmilons.java
@@ -11,27 +11,27 @@ public class BuildNationSmilons implements AddPlayers{
             members.add(new SmilonsHealer(nationName, tribeName, tribeLifePoints / 6));
             members.add(new SmilonsWarrior(nationName, tribeName, tribeLifePoints / 6));
             members.add(new SmilonsWizard(nationName, tribeName, tribeLifePoints / 6));
-            members.add(new SmilonsHealer(nationName, tribeName, tribeLifePoints / 6));
-            members.add(new SmilonsWarrior(nationName, tribeName, tribeLifePoints / 6));
-            members.add(new SmilonsWizard(nationName, tribeName, tribeLifePoints / 6));
+            members.add(new SmilonsHealer2(nationName, tribeName, tribeLifePoints / 6));
+            members.add(new SmilonsWarrior2(nationName, tribeName, tribeLifePoints / 6));
+            members.add(new SmilonsWizard2(nationName, tribeName, tribeLifePoints / 6));
         }
         if(tribeName.equals("Tribe2"))
         {
             members.add(new SmilonsHealer(nationName, tribeName, tribeLifePoints / 6));
             members.add(new SmilonsWarrior(nationName, tribeName, tribeLifePoints / 6));
             members.add(new SmilonsWizard(nationName, tribeName, tribeLifePoints / 6));
-            members.add(new SmilonsHealer(nationName, tribeName, tribeLifePoints / 6));
-            members.add(new SmilonsWarrior(nationName, tribeName, tribeLifePoints / 6));
-            members.add(new SmilonsWizard(nationName, tribeName, tribeLifePoints / 6));
+            members.add(new SmilonsHealer2(nationName, tribeName, tribeLifePoints / 6));
+            members.add(new SmilonsWarrior2(nationName, tribeName, tribeLifePoints / 6));
+            members.add(new SmilonsWizard2(nationName, tribeName, tribeLifePoints / 6));
         }
         if(tribeName.equals("Tribe3"))
         {
             members.add(new SmilonsHealer(nationName, tribeName, tribeLifePoints / 6));
             members.add(new SmilonsWarrior(nationName, tribeName, tribeLifePoints / 6));
             members.add(new SmilonsWizard(nationName, tribeName, tribeLifePoints / 6));
-            members.add(new SmilonsHealer(nationName, tribeName, tribeLifePoints / 6));
-            members.add(new SmilonsWarrior(nationName, tribeName, tribeLifePoints / 6));
-            members.add(new SmilonsWizard(nationName, tribeName, tribeLifePoints / 6));
+            members.add(new SmilonsHealer2(nationName, tribeName, tribeLifePoints / 6));
+            members.add(new SmilonsWarrior2(nationName, tribeName, tribeLifePoints / 6));
+            members.add(new SmilonsWizard2(nationName, tribeName, tribeLifePoints / 6));
         }
     }
 }
---
+++ SmilonsHealer2.java
@@ -0,0 +1,44 @@
+package Project02;
+
+import Project02.People;
+import Project02.PeopleType;
+
+public class SmilonsHealer2 extends Project02.People{
+    SmilonsHealer2(String nation, String tribe, int lifePoints)
+    {
+        super(nation, tribe, PeopleType.wizard, lifePoints);
+        myDescription = "\tSmilons Wizard2";
+    }
+    public int encounterStrategy(Project02.People otherPerson) {
+        int lifePoints = 0;
+        if (this.getNation() != otherPerson.getNation())
+        {
+            if (otherPerson.getLifePoints() < this.getLifePoints())
+            {
+                if (otherPerson.getType() == PeopleType.warrior) // run away
+                {
+                    lifePoints = -this.getLifePoints();
+                }
+                else // attack a wizard or healer but not with a lot of lifepoints
+                {
+                    lifePoints = (int) (this.getLifePoints()/4);
+                }
+            }
+        }
+        else
+        {
+            if (otherPerson.getLifePoints() < this.getLifePoints()) // heal a friend
+            {
+                lifePoints = (int) (this.getLifePoints() - otherPerson.getLifePoints() / 2);
+            }
+            else
+            {
+                lifePoints = 0;
+            }
+        }
+        return lifePoints;
+    }
+
+}
+
+
