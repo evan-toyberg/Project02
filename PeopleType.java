78ecd75fbbc8104444722d9eb7db6ee00b28a08d
Kyle Smilon <smilonk@moravian.edu>
2021-03-23 10:04:06 -0400

    JavaDoc for People Type.Java

--- PeopleType.java
+++ PeopleType.java
@@ -1,5 +1,9 @@
 package Project02;
-
+/**
+ * Using enum this class represents
+ * the different people located inside of a tribe.
+ * Each person has a description of their role in the game.
+ */
 public enum PeopleType
 {
     wizard  ("wizard"),
@@ -8,12 +12,18 @@ public enum PeopleType
 
     private String description;
 
+    /**
+     * @param types takes the current people type and sets the associated
+     *              description along with that type.
+     */
     PeopleType (String types)
     {
         description = types;
     }
 
-
+    /**
+     * @return the description of the people type.
+     */
     public String getDescription()
     {
         return description;
--- UpdateVersion/AddPlayers.java
+++
@@ -1,8 +0,0 @@
-package Project02;
-
-import java.util.ArrayList;
-
-public interface AddPlayers
-{
-    void add(ArrayList<People> members, String nationName, String tribeName, int tribepoints);
-}
